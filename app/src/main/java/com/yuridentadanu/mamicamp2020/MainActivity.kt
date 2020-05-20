fdd7c9202e7bf7f49d8abf7d9191739ecfd9a38d
Yuridenta Danu <muhammad.yuridenta.aulia.dzakidhanu@mail.ugm.ac.id>
2020-05-20 21:57:41 +0700

    challenge 4 : Add a Property

--- app/src/main/java/com/yuridentadanu/mamicamp2020/MainActivity.kt
+++ app/src/main/java/com/yuridentadanu/mamicamp2020/MainActivity.kt
@@ -2,11 +2,21 @@ package com.yuridentadanu.mamicamp2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    internal lateinit var btnTapMe: Button
+    internal lateinit var tvGameScore: TextView
+    internal lateinit var tvTimer: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnTapMe= findViewById(R.id.btn_tapMe)
+        tvGameScore = findViewById(R.id.tv_yourScore)
+        tvTimer = findViewById(R.id.tv_timeLeft)
+        
     }
 }
